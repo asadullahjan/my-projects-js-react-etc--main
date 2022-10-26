bb0c70509b1a1338ac486bf4bbbd1f0ca3da3d85
Asad <asadullah.jan@outlook.com>
2022-10-26 14:39:48 +0500

    used outlet for routing purpose also made route for navigation, home page, and shop page

--- react projects/crwn-clothing/src/App.js
+++ react projects/crwn-clothing/src/App.js
@@ -1,11 +1,21 @@
 import Home from "./routes/home/home.component";
 import {Routes, Route} from 'react-router-dom';
+import NavigationBar from "./routes/navigation-bar/navigation-bar.component";
+
+const Shop = () => {
+  return (
+    <h1>This is shop page</h1>
+  )
+}
 
 const App = () => {
   
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<NavigationBar/>}>
+        <Route index element={<Home/>}/>
+        <Route path="shop" element={<Shop/>}/>
+      </Route>
     </Routes>
   );
   
