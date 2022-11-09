99d7245ebb4ed1dce23b278738bfcd22752d8149
Asad <asadullah.jan@outlook.com>
2022-11-09 15:29:38 +0500

    added sign-in component

--- react projects/crwn-clothing/src/App.js
+++ react projects/crwn-clothing/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./routes/home/home.component";
 import {Routes, Route} from 'react-router-dom';
 import NavigationBar from "./routes/navigation-bar/navigation-bar.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Route path="/" element={<NavigationBar/>}>
         <Route index element={<Home/>}/>
         <Route path="shop" element={<Shop/>}/>
+        <Route path="sign-in" element={<SignIn/>}/>
       </Route>
     </Routes>
   );
